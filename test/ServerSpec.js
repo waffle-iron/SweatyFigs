5ddd42b4c4392ed7da23a386855f1a5cfb36342a
Robert Siferd <Rob.Siferd@gmail.com>
2016-08-31 16:44:10 -0700

    Added some basic server tests

--- test/ServerSpec.js
+++ test/ServerSpec.js
@@ -1,20 +1,32 @@
 var expect = require('chai').expect;
+var request = require('request');
+
+
+// TODO: We should have a set of config files somewhere that this can use.
+
 
 // Should be able to connect to the server.
 describe('', function() {
   describe('Basic Server Tests', function() {
-    it('Should foo', function(done) {
-      expect(1).to.equal(1);
-      done();
+    it('Should be able to connect to the server', function(done) {
+      request
+        .get('http://127.0.0.1:8000')
+        .on('response', (res) => {
+          expect(res.statusCode).to.equal(200);
+          done();
+        });
+    });
+
+    it('Should get a 404 for pages that do not exist', function(done) {
+      request
+        .get('http://127.0.0.1:8000/asdjaoisjd')
+        .on('response', (res) => {
+	  expect(res.statusCode).to.equal(404):
+          done();
+        });
     });
   });
 });
 
-// Should be able to get the index of the server.
-
-// Should be able to fetch all of the products from the server.
-
-// Should be able to search for a product on the server.
-
-// Server should give 404 for pages that don't exist.
+// Should be able to search recpies on the server. 
 
