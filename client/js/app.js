179ecab730585f57dd9b28954cb74e3f6ad6933b
Christopher Wong <chriskwong91@gmail.com>
2016-09-01 11:23:44 -0700

    Add new file app.js that has the overall module and children modules

---
+++ client/js/app.js
@@ -0,0 +1,13 @@
+// This file sets up the routes for fridgely
+
+angular.module('fridgely', [
+  'fridgely.recipes',
+  'fridgely.landing',
+  'fridegly.search',
+  'ngRoute'
+  ])
+
+  .config(function($routeProvider, $httpProvider) {
+
+
+  });
