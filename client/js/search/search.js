d807cebb68bfb80b0831a4f49eb315819aa74f84
Christopher Wong <chriskwong91@gmail.com>
2016-08-31 20:11:50 -0700

    Added search template controller with basic functions of text field and add button

---
+++ client/js/search/search.js
@@ -0,0 +1,29 @@
+
+angular.module('fridegly.search', [])
+  .controller('SearchController', function($scope, Search) {
+    $scope.data = {};
+
+    $scope.data.ingredients = [];
+
+    $scope.sendIngredients = function () {
+      Search.ingredients = $scope.data.ingredients;
+    };
+
+  });
+
+
+
+
+
+
+
+//This may be used in the future to autocomplete ingredient words,  can be drawn from a database full of the ingredient list
+
+// $('input.autocomplete').autocomplete({
+//   data: {
+//     "Apple": null,
+//     "Microsoft": null,
+//     "Google": 'http://placehold.it/250x250'
+//   }
+// });
+//
