b3e28d2ed42842b576224d11312169da33cdeac3
Robert Siferd <Rob.Siferd@gmail.com>
2016-09-01 12:08:59 -0700

    Modified comment

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,7 +1,7 @@
 module.exports = {
   extends: './node_modules/eslint-config-hackreactor/index.js',
   rules: {
-    // Rules here will override the 'hackreactor' configuration
+    // Rules here will override the default configuration
     // http://eslint.org/docs/rules/
   }
 };
