4c6365fcd69afd7a00ebe5accbfa1a6d09604199
Christopher Wong <chriskwong91@gmail.com>
2016-09-01 11:33:56 -0700

    Added the routes landing, recipes, search to the $routeProvider

--- client/js/app.js
+++ client/js/app.js
@@ -8,6 +8,21 @@ angular.module('fridgely', [
   ])
 
   .config(function($routeProvider, $httpProvider) {
-
+    $routeProvider
+      .when('/landing', {
+        templateUrl: 'js/landing/landing.html',
+        controller: 'LandingPageController'
+      })
+      .when('/recipes', {
+        templateUrl: 'js/recipes/recipes.html',
+        controller: 'RecipeController'
+      })
+      .when('/search', {
+        templateUrl: 'js/search/search.html',
+        controller: 'SearchController'
+      })
+      .otherwise({
+        redirectTo: '/landing'
+      });
 
   });
