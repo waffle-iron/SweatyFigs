5e73c101c04d145f758623432e633470866cfa3d
Brandon Tiqui <btiqui@gmail.com>
2016-08-31 19:48:15 -0700

    Add post route to api recipes

--- server/server.js
+++ server/server.js
@@ -1,6 +1,13 @@
 var express = require('express');
+var bodyParser = require('body-parser')
+
 var app = express();
 
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }))
+
+// parse application/json
+app.use(bodyParser.json());
 /**
 simulate database
 **/
@@ -24,7 +31,6 @@ var addItems = function(food) {
 
 addItems(['carrots', 'potatoes', 'rice']);
 
-console.log(items);
 
 // api/ingredients endpoint
 app.get('/api/ingredients', function (req, res) {
@@ -32,8 +38,15 @@ app.get('/api/ingredients', function (req, res) {
 });
 
 app.post('/add/:item', function (req, res) {
+	console.log('req', JSON.stringify(req.body));
 	addItem(req.params.item)
-  res.send(items);
+  res.send(JSON.stringify(req.body));
+});
+
+// calls api to get recipes
+app.post('/api/recipes', function (req, res) {
+	// prepare req.body for api
+	res.send('api called with ingredients');
 });
 
 app.get('/', function (req, res) {
