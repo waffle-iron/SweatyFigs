b0227610d53ed680ea649a410a0b4073fd1194a3
Robert Siferd <Rob.Siferd@gmail.com>
2016-08-31 20:13:26 -0700

    Updated server to match linter

--- server/server.js
+++ server/server.js
@@ -1,10 +1,10 @@
 var express = require('express');
-var bodyParser = require('body-parser')
+var bodyParser = require('body-parser');
 
 var app = express();
 
 // parse application/x-www-form-urlencoded
-app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.urlencoded({ extended: false }));
 
 // parse application/json
 app.use(bodyParser.json());
@@ -14,19 +14,19 @@ simulate database
 var items = [];
 
 var addItem = function(item) {
-	var itemObj = {};
-	if (item) {
-		itemObj._id = items.length;
-		itemObj.name = item;
-		items.push(itemObj);
-	}
+  var itemObj = {};
+  if (item) {
+    itemObj._id = items.length;
+    itemObj.name = item;
+    items.push(itemObj);
+  }
 };
 
 // add array of items
 var addItems = function(food) {
-	food.forEach(function(item) {
-		addItem(item);
-	})
+  food.forEach(function(item) {
+    addItem(item);
+  });
 };
 
 addItems(['carrots', 'potatoes', 'rice']);
@@ -38,15 +38,15 @@ app.get('/api/ingredients', function (req, res) {
 });
 
 app.post('/add/:item', function (req, res) {
-	console.log('req', JSON.stringify(req.body));
-	addItem(req.params.item)
+  console.log('req', JSON.stringify(req.body));
+  addItem(req.params.item);
   res.send(JSON.stringify(req.body));
 });
 
 // calls api to get recipes
 app.post('/api/recipes', function (req, res) {
-	// prepare req.body for api
-	res.send('api called with ingredients');
+  // prepare req.body for api
+  res.send('api called with ingredients');
 });
 
 app.get('/', function (req, res) {
